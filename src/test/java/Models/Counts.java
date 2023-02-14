1bece967135458cb1bb05dad6ed907617db04d7f
Evgeniy <tinkerbox@yandex.by>
2023-02-14 23:55:57 +0300

    Rest API

---
+++ src/test/java/Models/Counts.java
@@ -0,0 +1,25 @@
+package Models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Counts {
+    private int cases, suites, milestones;
+    private Runs runs;
+    private Defects defects;
+
+    @Data
+    @Builder
+    public static class Runs {
+        private int total, active;
+
+    }
+    @Data
+    @Builder
+    public static class Defects {
+        private int total, open;
+
+    }
+}
---
+++ src/test/java/Models/Project.java
@@ -0,0 +1,12 @@
+package Models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Project {
+    private String title;
+    private String code;
+    private Counts counts;
+}
